e3f2f1097e41e9d7281316c0362da7db9ef814bb
IT25103841 <it25103841@my.sliit.lk>
2026-05-24 18:57:59 +0530

    Update CheckoutController.java

--- src/main/java/com/grocery/controller/CheckoutController.java
+++ src/main/java/com/grocery/controller/CheckoutController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class CheckoutController {
+public class CheckoutController { 
     @Autowired
     private CartService cartService;
 
