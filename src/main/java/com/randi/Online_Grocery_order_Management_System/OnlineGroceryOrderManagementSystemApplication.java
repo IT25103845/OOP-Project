a6e487468b3f06424582ecd2de2464e0790464e9
IT25103845 <IT25103845@my.sliit.lk>
2026-05-25 12:27:50 +0530

    Update OnlineGroceryOrderManagementSystemApplication.java

--- src/main/java/com/randi/Online_Grocery_order_Management_System/OnlineGroceryOrderManagementSystemApplication.java
+++ src/main/java/com/randi/Online_Grocery_order_Management_System/OnlineGroceryOrderManagementSystemApplication.java
@@ -2,7 +2,7 @@ package com.randi.Online_Grocery_order_Management_System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+      
 @SpringBootApplication
 public class OnlineGroceryOrderManagementSystemApplication {
 
