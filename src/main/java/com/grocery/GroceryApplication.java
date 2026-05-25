61e11bde0965b9396b8dfcfcbc16f5c2954de706
IT25103841 <it25103841@my.sliit.lk>
2026-05-26 00:59:40 +0530

    Update GroceryApplication.java

--- src/main/java/com/grocery/GroceryApplication.java
+++ src/main/java/com/grocery/GroceryApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class GroceryApplication {  
-
+ 
     public static void main(String[] args) {
         SpringApplication.run(GroceryApplication.class, args);
         System.out.println("==============================================");
