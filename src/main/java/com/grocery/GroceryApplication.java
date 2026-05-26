e3b5894e805226460e52990ff639b1cf849a154f
it25103840 <it25103840@my.sliit.lk>
2026-05-26 10:22:08 +0530

    Format GroceryApplication.java for consistency

--- src/main/java/com/grocery/GroceryApplication.java
+++ src/main/java/com/grocery/GroceryApplication.java
@@ -3,7 +3,7 @@ package com.grocery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
 public class GroceryApplication {
     public static void main(String[] args) {
         SpringApplication.run(GroceryApplication.class, args);
